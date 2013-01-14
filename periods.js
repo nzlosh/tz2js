1b25aae5a0f57f489f5db94ffa4363ee0589f975
Carlos <nzlosh@yahoo.com>
2013-01-14 23:24:09 +0100

    Proof of concept ideas added to periods.js Added more parsing code to tz2js script.

--- periods.js
+++ periods.js
@@ -3,10 +3,10 @@
    between 9am - 10am (range between time)
    Only Mondays (range between weekdays)
    1st Tuesday of each month ( repeat monthly,daily, hourly, weekly, fortnightly)
-   1st Tuesday of each week ( repeat weekly, fortnightly ) 
+   1st Tuesday of each week ( repeat weekly, fortnightly )
    even weeks, odd weeks, modular x weeks  (modular calculation
    2nd & 3rd Wednesday between June and August (repeat between ranges)
-   
+
    THIS CODE EXPECTS THE LOCALTIME TO BE SET TO UTC+0.
 */
 function htmlLine(msg)
@@ -25,7 +25,7 @@ function periods()
 function timeBetweenRange(lower, upper)
 {
 	var now = new Date();
-	
+
 	if (lower === undefined )
 	{
 		lower = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,1);
@@ -34,7 +34,7 @@ function timeBetweenRange(lower, upper)
 	{
 		upper = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23,59,59);
 	}
-	
+
 	l = dateToDayEpoch(lower);
 	u = dateToDayEpoch(upper);
 	n = dateToDayEpoch(now);
@@ -47,16 +47,64 @@ function dateToDayEpoch(d)
 	return d.getUTCHours()*60000 + d.getUTCMinutes()*1000 + d.getUTCSeconds();
 }
 
+function RangeRule(r)
+{
+	default_rule = {
+		weeknames: ["Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday"],
+		now: new Date(),
+		execute: true,
+		times: ["00:00:01", "23:59:59"],
+		weekddays: "",
+		months: ["jan", "dec"],
+		timezone: "europe/paris"
+	};
+	if ( rule.length < 3 )
+	{
+		htmlLine("Error, insufficient information to check range.");
+		return false;
+	}
+
+	switch ( r.length )
+	{
+		case 3:
+			execute = r[0];
+			times = r[1];
+			timezone = r[2];
+			break;
+		case 4:
+			execute = r[0];
+			times = r[1];
+			weekddays = r[2];
+			timezone = r[3];
+			break;
+		case 5:
+			execute = r[0];
+			times = r[1];
+			weekddays = r[2];
+			months = r[3];
+			timezone = r[4];
+			break;
+	} 
+
+// [ false, ["00:00:01", "23:59:59"], ["monday","friday"], ["jan","dec"], "europe/paris" ]);
+}
+
 periods();
+
+
+RangeRule([ false, ["00:00:01", "23:59:59"], ["monday","friday"], ["jan","dec"], "europe/paris" ]);
+
 /*
+Possible range / period usage formats:
+
 t = new TimeRange ("00:00:01","23:59:59") // 00h00m01s to 23h59m59s
 tr = new TimeList ([t, new TimeRange("12:00:01-20:01:30")])
 w = new WeekdayRange ("Fri", "Sun") // Friday to Sunday
 wdr = new WeekdayRepeat (["Mon","Wed", x]) // Monday, Wednesday, Friday to Sunday
 woy new WeekofYear([1,2,3,(4-50)])
-* 
+
 WeeklyRangeRule ({ x: true, h:{l:[00,00,01], u:[23,59,59]}, d:["monday","friday"], m:["jan","dec"], tz:"europe/paris" });
-WeeklyRangeRule ([ false, [[00,00,01], [23,59,59]], ["monday","friday"], ["jan","dec"], "europe/paris" ]);
+WeeklyRangeRule ([ false, ["00:00:01", "23:59:59"], ["monday","friday"], ["jan","dec"], "europe/paris" ]);
 
 RepeatRule (["LastDayOfMonth", "Sunday"])
 RepeatRule ([50, "WeekOfYear"])
--- tz2js.py
+++ tz2js.py
@@ -1,57 +1,156 @@
 #!/usr/bin/env python
 
+# The MIT License (MIT)
+#
+# Copyright (c) 2013 Carlos (nzlosh@yahoo.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+
 # Required modules
 import os, sys, re, json
-
-# This file contains a table with the following columns:
-# 1.  ISO 3166 2-character country code.  See the file `iso3166.tab'.
-# 2.  Latitude and longitude of the zone's principal location
-#     in ISO 6709 sign-degrees-minutes-seconds format,
-#     either +-DDMM+-DDDMM or +-DDMMSS+-DDDMMSS,
-#     first latitude (+ is north), then longitude (+ is east).
-# 3.  Zone name used in value of TZ environment variable.
-# 4.  Comments; present if and only if the country has multiple rows.
-
 
 tzdata_version = "2012j"
 tzpath = os.path.join(sys.argv[1],"tzdata"+tzdata_version)
 
-"africa",
-"antarctica",
-"asia",
-"australasia",
-"backward",
-"etcetera",
-"europe",
-"leapseconds",
-"northamerica",
-"pacificnew",
-"solar87",
-"solar88",
-"solar89",
-"southamerica",
-"systemv",
+months = [
+	"january",
+	"february",
+	"march",
+	"april",
+	"may",
+	"june",
+	"july",
+	"august",
+	"september",
+	"october",
+	"november",
+	"december"
+]
 
-
+zone_files = [
+	"africa",
+	"antarctica",
+	"asia",
+	"australasia",
+	#~ "backward",
+	"etcetera",
+	"europe",
+	#~ "leapseconds",
+	"northamerica",
+	"pacificnew",
+	#~ "solar87",
+	#~ "solar88",
+	#~ "solar89",
+	"southamerica",
+	#~ "systemv"
+]
 
 def parseZoneFile():
+	"""
+	Information about the file being parsed:
+
+	This file contains a table with the following columns:
+	1.  ISO 3166 2-character country code.  See the file `iso3166.tab'.
+	2.  Latitude and longitude of the zone's principal location
+		in ISO 6709 sign-degrees-minutes-seconds format,
+		either +-DDMM+-DDDMM or +-DDMMSS+-DDDMMSS,
+		first latitude (+ is north), then longitude (+ is east).
+	3.  Zone name used in value of TZ environment variable.
+	4.  Comments; present if and only if the country has multiple rows.
+	"""
 	zone_file = "zone.tab"
 	z = {}
 	zones = open( os.path.join(tzpath,zone_file), "r")
+
 	for line in zones.readlines():
 		# Skip full line comments
 		if re.search(r"^\w*#", line):
 			continue
+
 		# strip carriage return, remove end of line comments, clean whitespace
 		# after 1st split and then split the remainder as a tab delimited record.
 		rec = line.strip("\n").split("#",1)[0].strip().split("\t")
 
-		if z.has_key(rec[0]):
-			z[rec[0]].append(rec[2])
+		# The only information required are Zone names.  Which are split
+		# into Area and Location.
+		area, location = rec[2].split("/",1)
+		if z.has_key(area):
+			z[area][location] = {}
 		else:
-			z[rec[0]] = [rec[2]]
+			z[area] = {}
+			z[area][location] = {}
 
 	zones.close()
 	return z
 
-print json.dumps(parseZoneFile())
+#~ print json.dumps(parseZoneFile())
+
+def parseRuleZoneFiles(zone_file):
+	"""
+	Information about the files being parsed:
+
+	If the Rule From > now and now < Rule To, apply the rule.
+	# Rule	NAME	FROM	TO	TYPE	IN	ON	AT	SAVE	LETTER/S
+
+
+	If the Zone UNTIL is < now ignore rule.
+	# Zone	NAME		GMTOFF	RULES	FORMAT	[UNTIL]
+	# Northern Territory
+	#Zone Australia/Darwin	 8:43:20 -	LMT	1895 Feb
+	#			 9:00	-	CST	1899 May
+	#			 9:30	Aus	CST
+
+	Link's to an already established timezone.
+	Link	Antarctica/McMurdo	Antarctica/South_Pole
+	"""
+	context=''
+	zfh = open(os.path.join(tzpath, zone_file), "r")
+	for line in zfh.readlines():
+		# Skip full line comments
+		if re.search(r"^\w*#", line):
+			continue
+
+		# Remove carriage returns and trailing comments.
+		line = line.strip("\n").split('#',1)[0]
+
+		# Skip empty lines.
+		if re.search(r"^$", line):
+			continue
+
+		# Split the line into each field of the record.
+		s = line.split("\t")
+
+		# Get the context of the line Zone, Rule or Link.
+		if len(s[0]):
+			context = s[0]
+
+		# Detect malformed records and attempt to fix them.
+		if context.lower().find("zone") == 0 and len(s) < 5:
+			print "***",line
+		elif context.lower().find("rule") == 0 and len(s) != 10:
+			print "***",line
+		elif context.lower().find("link") == 0and len(s) != 3:
+			print "%%%", line
+		else:
+			print line
+	zfh.close()
+
+for z in zone_files:
+	parseRuleZoneFiles(z)
