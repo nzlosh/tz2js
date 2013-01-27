370a47bed144004bc6de1fb621b91a0caeb9827e
Carlos <nzlosh@yahoo.com>
2013-01-27 22:32:34 +0100

    Work in progress - added class for Rule and Zone entries.

--- periods.js
+++ periods.js
@@ -7,8 +7,15 @@
    even weeks, odd weeks, modular x weeks  (modular calculation
    2nd & 3rd Wednesday between June and August (repeat between ranges)
 
-   THIS CODE EXPECTS THE LOCALTIME TO BE SET TO UTC+0.
+
+---- Date / Time / Timezone
+
+	Javascripts Date object calculations are done using milliseconds
+	epoch timestamp.
+
 */
+
+
 function htmlLine(msg)
 {
 	document.write("<p>",msg,"</p>");
@@ -16,19 +23,20 @@ function htmlLine(msg)
 
 function periods()
 {
-	var d = new Date();
+	var d = new Date(0);
 	htmlLine(d);
 	htmlLine(d.toUTCString());
-	htmlLine(timeBetweenRange());
+	htmlLine( new Date(Date.UTC(2012,02,30)) );
+	htmlLine( "Is the date within the range? " + betweenRange() );
 }
 
-function timeBetweenRange(lower, upper)
+function betweenRange(lower, upper)
 {
 	var now = new Date();
 
 	if (lower === undefined )
 	{
-		lower = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,1);
+		lower = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0);
 	}
 	if ( upper === undefined )
 	{
@@ -38,27 +46,23 @@ function timeBetweenRange(lower, upper)
 	l = dateToDayEpoch(lower);
 	u = dateToDayEpoch(upper);
 	n = dateToDayEpoch(now);
+
+	htmlLine( "</p><h2>lower</h2><p> Timestamp = " + l );
+	htmlLine( " Localtime = " + l);
+	htmlLine( " UTC time = " + l.toUTCString);
+
+
 	return l < n && n < u;
 }
 
 function dateToDayEpoch(d)
 {
-	htmlLine(d.toUTCString());
-	return d.getUTCHours()*60000 + d.getUTCMinutes()*1000 + d.getUTCSeconds();
+	return d.getTime();
 }
 
 function RangeRule(r)
 {
-	default_rule = {
-		weeknames: {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6 ,"sun":0 },
-		now: new Date(),
-		execute: true,
-		times: [[0,0,1], [23,59,59]],
-		weekddays: "",
-		months: ["jan":0, "feb":1, "mar":2, "apr":3, "may":4, "jun":5,
-				 "jul":6, "aug":7, "sep":8, "oct":9, "nov":10, "dec":11],
-		timezone: "europe/paris"
-	};
+
 	if ( r.length < 3 )
 	{
 		htmlLine("Error, insufficient information to check range.");
@@ -76,6 +80,40 @@ periods();
 
 RangeRule([ false, "00:00:01-23:59:59", ["monday","friday"], ["jan","dec"], "europe/paris" ]);
 
+
+var rule = {
+	weeknames: {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6 ,"sun":0 },
+	months: {"jan":0, "feb":1, "mar":2, "apr":3, "may":4, "jun":5,
+			 "jul":6, "aug":7, "sep":8, "oct":9, "nov":10, "dec":11},
+	now: new Date(),
+	execute: true,
+	times: [[0,0,1], [23,59,59]],
+	weekddays: "",
+	timezone: "europe/paris"
+};
+
+function timerange(r) {
+	this.range = r,
+	this.begin = {h:0,m:0,s:0},
+	this.end = {h:23,m:59,s:59},
+	this.limit = {min:0, max:86400000}
+	
+	// private method 
+	var _parseRange = function(){
+		if ( this.range !== undefined ) {
+			// todo
+		}
+	}
+}
+
+
+var weekrange = {
+
+}
+var monthrange = {
+
+}
+
 /*
 Possible range / period usage formats:
 
@@ -88,6 +126,6 @@ woy new WeekofYear([1,2,3,(4-50)])
 WeeklyRangeRule ({ x: true, h:{l:[00,00,01], u:[23,59,59]}, d:["monday","friday"], m:["jan","dec"], tz:"europe/paris" });
 WeeklyRangeRule ([ false, ["00:00:01", "23:59:59"], ["monday","friday"], ["jan","dec"], "europe/paris" ]);
 
-RepeatRule (["LastDayOfMonth", "Sunday"])
+RepeatRule (["<=31", "Sunday"])
 RepeatRule ([50, "WeekOfYear"])
 */
--- tz2js.py
+++ tz2js.py
@@ -24,43 +24,147 @@
 
 
 # Required modules
-import os, sys, re, json
+import os, sys, re, json, time
 
 tzdata_version = "2012j"
 tzpath = os.path.join(sys.argv[1],"tzdata"+tzdata_version)
 
-months = [
-	"january",
-	"february",
-	"march",
-	"april",
-	"may",
-	"june",
-	"july",
-	"august",
-	"september",
-	"october",
-	"november",
-	"december"
-]
+months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
+days = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ]
 
 zone_files = [
-#	"africa",
-#	"antarctica",
-#	"asia",
-#	"australasia",
+	"africa",
+	"antarctica",
+	"asia",
+	"australasia",
 	#~ "backward",
-#	"etcetera",
-#	"europe",
+	"etcetera",
+	"europe",
 	#~ "leapseconds",
-#	"northamerica",
-#	"pacificnew",
+	"northamerica",
+	"pacificnew",
 	#~ "solar87",
 	#~ "solar88",
 	#~ "solar89",
 	"southamerica",
 	#~ "systemv"
 ]
+
+
+
+
+
+class tzRule(object):
+	"""
+	Holds a timezone rule.  The expected format is a list which contains
+	the [Rule, NAME, FROM, TO, TYPE, IN, ON, AT, SAVE, LETTER/S]
+	"""
+	def __init__(self, name, year_from, rule_type, year_to, month_in, day_on, time_at, save, letters):
+		self.name = name
+		self.setYearFrom(year_from)
+		self.setYearTo(year_to)
+		self.setMonthIn(month_in)
+		self.getDayOn(day_on)
+		self.time_at = time_at
+		self.rule_type = rule_type
+		self.save = save
+		self.setLetters(letters)
+
+
+	def setFromYear(self):
+		# We only test times equal to or greater than the present.
+		if int(z[2]) <= time.gmtime()[0] <= int(z[3]):
+			print "Fields: %d" %len(z), "*** Name:", z[1], "From: ", z[2], "to %s/%s/%s" % (z[3], months.index(z[5]), z[6]), "^^^ TYPE:",z[4], "AT:", z[7],"SAVE:",z[8], z[9].strip()
+
+
+	def setYearFrom(self, year_from):
+		"""
+		Expected input: YYYY formatted year.
+		"""
+		if year_from.isdigit():
+			self.year_from = int(year_from)
+		else:
+			raise "Year From isn't a digit!"
+
+
+	def getYearFrom(self):
+		return self.year_from
+
+
+	def setYearTo(self, year_to):
+		if year_to == "only":				# Transform TO "only" to the equivalent year as FROM
+			self.year_to = self.year_from
+		elif year_to == "max":				# Transform TO "max" arbitrarily to 40 years in the future.
+			self.year_to = 2050
+		else:
+			self.year_to = int(year_to)		# cast to integer
+
+
+	def getYearTo(self):
+		return self.year_to
+
+
+	def setMonthIn(self, month_in):
+		self.month_in = months.index(month_in)+1
+
+
+	def getMonthIn(self):
+		return self.month_in
+
+
+	def getDayOn(self, day_on):
+		if day_on.isdigit():
+			self.day_on = int(day_on)
+		else:
+			# confirm the format is valid then calculate the day of the month.
+			self.day_on = "f(%s)" % day_on
+			try:
+				day, comp, dom = re.search('(\w+)(\W+)(\d+)',day_on).groups()
+				print day, comp, dom
+			except(AttributeError):
+				try:
+					day = re.search('last(\w+)', day_on).groups()[0]
+					print day,"<",31
+				except:
+					print "ZOOOOOT!", day_on
+
+
+	def setLetters(self, letters):
+		self.letters = letters.strip()
+		if self.letters == "-":
+			self.letters = ""
+
+
+
+	def __str__(self):
+		return "Rule: %s %s -> %s/%s/%s@%s Type: %s Save: %s Letter: %s" % (
+			self.name,
+			self.year_from,
+			self.year_to,
+			self.month_in,
+			self.day_on,
+			self.time_at,
+			self.rule_type,
+			self.save,
+			self.letters
+		)
+
+
+
+class tzZone(object):
+	"""
+	[Zone, NAME, GMTOFF, RULES, FORMAT, [UNTIL]] or
+	[GMTOFF, RULES, FORMAT, [UNTIL]]
+	"""
+	def __init__(name, gmt_off, rules, zone_format, until):
+		self.name
+		self.gmt_off
+		self.rules
+		self.zone_format
+		self.until
+
+
+
 
 def parseZoneFile():
 	"""
@@ -100,7 +204,8 @@ def parseZoneFile():
 	zones.close()
 	return z
 
-#~ print json.dumps(parseZoneFile())
+
+
 
 def parseRuleZoneFiles(zone_file):
 	"""
@@ -125,11 +230,12 @@ def parseRuleZoneFiles(zone_file):
 	context=''
 	area = ''
 	location = ''
+	tmp = []
 
 	zfh = open(os.path.join(tzpath, zone_file), "r")
 	for line in zfh.readlines():
 		# Skip full line comments
-		if re.search(r"^\w*#", line):
+		if re.search(r"^\s*#", line):
 			continue
 
 		# Remove carriage returns and trailing comments.
@@ -139,37 +245,63 @@ def parseRuleZoneFiles(zone_file):
 		if re.search(r"^$", line):
 			continue
 
-		# Split the line into each field of the record.
+		# Identify the type of line to process.
 		x = re.search('^(.)', line)
 
 		# Get the context of the line either: Zone, Rule or Link.
 		if x.groups()[0] != '\t':
 			context = x.groups()[0]
 
-		# Detect malformed records and attempt to fix them.
+		# Extract the fields from the line.
 		if context.lower() == "z":
 			if x.groups()[0].lower() == "z":
 				r = re.search('^(Z[^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+(.*)$', line)
 				if r:
-					print len(r.groups()), r.groups()
-			elif re.search('\s', x.groups()[0]):
+					#~ print len(r.groups()), r.groups()
+					tmp.append( list(r.groups()) )
+			elif re.search('^\s', x.groups()[0]):
 				# 			-4:32:36 1:00	BOST	1932 Mar 21 # Bolivia ST
 				r = re.search('^\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)(.*)$', line)
 				if r:
-					print len(r.groups()), r.groups()
+					#~ print len(r.groups()), r.groups()
+					tmp.append( list(r.groups()) )
 			else:
-				print "UNKNOWN ZONE FORMAT!"
+				print "UNKNOWN ZONE FORMAT!", line
 		elif context.lower() == "r":
 			r = re.search("^(R[^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)(.*)", line)
 			if r:
-				print len(r.groups()), r.groups()
+				#~ print len(r.groups()), r.groups()
+				tmp.append( list(r.groups()) )
 			else:
 				print "UNKOWN RULE FORMAT!", line
 		elif context.lower() == "l":
-			print line
+			r = re.search("^(L[^\s]+)\s+([^\s]+)\s+([^\s]+)", line)
+			if r:
+				#~ print len(r.groups()), r.groups()
+				tmp.append( list(r.groups()) )
+			else:
+				print "UNKNOWN LINK FORMAT!", line
 		else:
-			print "UNKNOWN LINE"
+			print "UNKNOWN LINE!", line
 	zfh.close()
 
+	return tmp
+
+
+zone_list = parseZoneFile();
+
+zone_result = []
 for z in zone_files:
-	parseRuleZoneFiles(z)
+	zone_result.append(parseRuleZoneFiles(z))
+
+#print json.dumps(zone_result)
+#print json.dumps(parseZoneFile())
+
+
+for zr in zone_result:
+	for z in zr:
+		if z[0].lower() == "rule":
+			a_rule = tzRule(z[1],z[2],z[4],z[3],z[5],z[6],z[7],z[8],z[9])
+			print a_rule
+		else:
+			print z
