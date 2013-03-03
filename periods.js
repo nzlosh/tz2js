189d0d8c5850b3f59e80bbdf948334c12e1eaaf9
Carlos <nzlosh@yahoo.com>
2013-03-03 21:54:53 +0100

    -- work in progress --   * Integrated code from PDA into periods.js   * Started to define a timezone aware date object.

--- periods.js
+++ periods.js
@@ -1,32 +1,51 @@
-// Check if a period is within a given range.
-/* EXAMPLES
-   between 9am - 10pm (range between time)
-   Only Mondays (range between weekdays)
-   1st Tuesday of each month ( repeat monthly,daily, hourly, weekly, fortnightly)
-   1st Tuesday of each week ( repeat weekly, fortnightly )
-   even weeks, odd weeks, modular x weeks  (modular calculation
-   2nd & 3rd Wednesday between June and August (repeat between ranges)
-
----- Date / Time / Timezone
-
-    Javascripts Date object calculations are done using milliseconds
-    epoch timestamp.
-
-Resource references:
-* http://howtonode.org/prototypical-inheritance
-
+/* periods.js
+ * ==========
+ * Checks the current time is within a given time period based on a set
+ * of criteria which determine time of day, day of week, day of month,
+ * month, week of year, year, timezone and execution state.
+ *
+ * Examples are:
+ * between 9am - 10pm (range between time)
+ * new Period({time: ["9","10"]});
+ *
+ * Only Mondays (range between weekdays)
+ * new Period({dow: ["mon","mon"]});
+ *
+ *
+ * Currently unimplemented ranges
+ * ------------------------------
+ * 1st Tuesday of each month ( repeat monthly, weekly, fortnightly)
+ *
+ * Even weeks, odd weeks, modular x weeks  (modular calculation)
+ *
+ * 2nd & 3rd Wednesday between June and August (repeat between ranges)
+ *
+ *
+ * Date / Time / Timezone
+ * ======================
+ * Javascript's Date object calculations are done using milliseconds timestamp
+ * since midnight 1st Jan 1970.
+ *
+ *
+ * Resource references
+ * ===================
+ * http://howtonode.org/prototypical-inheritance
 */
 
 
 // Global variables.
 days = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6 ,"sun":0 };
 months = {"jan":0, "feb":1, "mar":2, "apr":3, "may":4, "jun":5, "jul":6, "aug":7, "sep":8, "oct":9, "nov":10, "dec":11};
+execute_policy = true;      // Set the default execution policy in the event no Periods match or are defined.
 
 
 /*
  * A logging object inspired by the Python's logger class.
  * Logs to null, console or web page.
- */
+ * Levels are determined by an integer value.  The higher the value, the
+ * more verbose the logging becomes.
+ *
+*/
 var Log = function(level){
     this.level = level;
     this.levels = {"debug": 10, "warn": 20, "info": 30};
@@ -52,82 +71,67 @@ Log.prototype.debug = function debug(msg) {
 Log.prototype.info = function info(msg) {
     this.active_info("INFO", msg);
 }
-// Create the logger object with the required verbosity.
+
+// Create the logger object immediately so it's available for the function
+// definitions that follow directly below.
 log = new Log(30);
 
 
-
-// This is taken from http://tech.karbassi.com/2009/12/18/object-type-in-javascript/
+/*
+ * getType
+ * =======
+ * This is taken from http://tech.karbassi.com/2009/12/18/object-type-in-javascript/ and
+ * provides a clean implementation for extracting the object type as a string.
+*/
 function getType(obj){
     if (obj === undefined) { return 'undefined'; }
     if (obj === null) { return 'null'; }
     return Object.prototype.toString.call(obj).split(' ').pop().split(']').shift().toLowerCase();
 }
-
-/* The prototype method isn't used because it interfares.
+/* The prototype method is part of the implementation but it causes undesirable
+ * effects when integrated into the Zone and Rule objects.
+*/
+/*
 Object.prototype.getType = function(){
     return Object.prototype.toString.call(this).split(' ').pop().split(']').shift().toLowerCase();
 };
 */
 
-
-
 /*
- * "Split First" splits a string on the first occurrence
- * of the given character.
+ * splitOnFirst
+ * ============
+ * Split a string on the first occurrence of the given character.
  *
- * Returns the string argument if no occurrence is found,
- * Returns an array with the split .
+ * @split_string: the string to attempt to split.
+ * @split_char: the character to split on.
+ *
+ * If the split_char isn't found in the split_string argument, the
+ *   split_string argument is returned unmodified.
+ * If an occurence of split_char is found in split_string, an array containing
+ *   the left hand side and right hand side strings is returned.
  *
 */
-function splitOnFirst(splitee, split_char) {
-    pos = splitee.indexOf(split_char);
+function splitOnFirst(split_string, split_char) {
+    pos = split_string.indexOf(split_char);
     if ( pos == -1 ) {
-        return splitee;
+        return split_string;
     }
-    return [splitee.substr(0, pos), splitee.substr(pos+1)];
+    return [split_string.substr(0, pos), split_string.substr(pos+1)];
 }
 
 
 
-
-
-function htmlLine(msg, tag)
-{
-    tag = tag == undefined ? "p" : tag;
-    document.write("<"+tag+">",msg,"</"+tag+">");
-}
-
-
-function betweenRange(lower, upper)
-{
-    var now = new Date();
-
-    if (lower === undefined )
-    {
-        lower = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0,0,0);
-    }
-    if ( upper === undefined )
-    {
-        upper = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23,59,59);
-    }
-
-    l = dateToDayEpoch(lower);
-    u = dateToDayEpoch(upper);
-    n = dateToDayEpoch(now);
-
-    htmlLine( "</p><h2>lower</h2><p> Timestamp = " + l );
-    htmlLine( " Localtime = " + l);
-    htmlLine( " UTC time = " + l.toUTCString);
-
-
-    return l < n && n < u;
-}
-
-
-/*
- * Period accepts a key value pair structure to over-ride it's default parameters.
+/* Period
+ * ======
+ *
+ * Period accepts a key value pair structure to override it's default parameters.
  * The parameters are then used to determine if the current time is with the defined period.
+ *
+ * Valid keys:
+ *   execute: boolean to indicate if the period is execution or non-execution.
+ *   time: an array containing the start and end times for a period.
+ *   day: the
+ * .... to do ....
  */
 function Period(kwargs){
     this.default_period = {
@@ -146,7 +150,7 @@ function Period(kwargs){
         log.debug(k + " : " + this.default_period[k]);
         this.default_period[k] = kwargs[k];
     }
-    // Apply logic to supplied agruments
+    // Call functions to process period arguments.
     this.parseTimeZone();
     this.parseYear();
     this.parseWeek();
@@ -154,13 +158,22 @@ function Period(kwargs){
     this.parseMonth();
     this.parseDay();
     this.parseTime();
-}
+};
 Period.prototype.toString = function toString() {
     s = "";
     for ( var k in this.default_period ) {
         s += "  " + k +": " + this.default_period[k] ;
     }
     return s;
+}
+/* insideBoundaries
+ * ================
+ * Given a date/time with timezone extensions, determine if it falls within
+ * the boundaries of the Period's constraints.
+*/
+function insideBoundaries(check_date)
+{
+    throw "insideBoundaries Unimplemented";
 }
 Period.prototype.parseTimeZone = function paseTimeZone() {
     var tz = this.default_period["tz"];
@@ -183,8 +196,7 @@ Period.prototype.parseWeek = function parseWeek() {
 
     this.default_period["week"] = week;
 }
-/*
- * Period method parseTime
+/* Period method parseTime
  * Brief: Accept an array containing two elements of either type array or string.
  * A element array may contain between 1 and 4 elements, a string may be of the
  * format "[h]h[:[m]m][:[s]s][:[t][t]t]" h=hour, m=minute, s=second, t=millisecond.
@@ -197,8 +209,7 @@ Period.prototype.parseTime = function parseTime() {
 
     this.default_period["time"] = time;
 }
-/*
- * Period method parseDay
+/* Period method parseDay
  * Brief: Takes a list and ensures the resulting internal
  * variable is updated with an integer representation of the day.
  *
@@ -360,38 +371,94 @@ Period.prototype._validateDayOfMonth = function _validateDayOfMonth(dom) {
     return dom;
 }
 
-
-var period1 = new Period( );
-log.info( period1.toString() );
-
-now = new Date();
-msPerLeapYear = 126230400000;
-msPerYear = 31536000000;
-msPerDay = 86400000;
-msPerHour = 3600000;
-msPerMin = 60000;
-
-
-/* Start to work on the calculations for converting epoch time to Y/M/D h:m:s:ms time. */
-msToday = now.getTime() % msPerDay;
-
-msThisHour = msToday % msPerHour;
-hour = Math.floor( msToday / msPerHour );
-
-msThisMinute = msToday % msPerMin;
-minute = Math.floor( msThisHour / msPerMin );
-
-second = Math.floor( msThisMinute / 1000);
-millisecond = msThisMinute % 1000;
-
-htmlLine( hour +":"+ minute +":"+ second +":"+ millisecond);
-htmlLine( Math.floor(now.getTime() / msPerYear) );
-
-
+//**********************************************************************
 /* Test Date object with tz data values" */
 function tzDate(date, tz, dst) {
-    this.date = new Date();
+    this.now = new Date();
+    // these variables aren't all require but they're included in a previsory capacity.
+    this.msPerLeapYear = 126230400000;
+    this.msPerYear = 31536000000;
+    this.msPerDay = 86400000;
+    this.msPerHour = 3600000;
+    this.msPerMin = 60000;
+    /* Start to work on the calculations for converting epoch time to Y/M/D h:m:s:ms time. */
+    this.msToday = this.now.getTime() % this.msPerDay;
+    this.msThisHour = this.msToday % this.msPerHour;
+    this.hour = Math.floor( this.msToday / this.msPerHour );
+    this.msThisMinute = this.msToday % this.msPerMin;
+    this.minute = Math.floor( this.msThisHour / this.msPerMin );
+    this.second = Math.floor( this.msThisMinute / 1000);
+    this.millisecond = this.msThisMinute % 1000;
+
+    // millisecond based calculations *********
+    this.msPerWeek = 7 * msPerDay;
+    this.msPerLeapYear = msPerDay * ( 4 * 365 + 1);
+
+    // day based calculations *********
+    this.msOfYear = this.now.getTime() % this.msPerYear - ( this.totalLeptDays * this.msPerDay ) ;
+    this.msToday = this.msOfYear % this.msPerDay;
+    this.Year = ( this.msOfYear - this.msToday ) / this.msPerDay;
+    this.dayOfYear = (this.msOfYear - this.msToday)  / this.msPerDay;
+
+    //this.leapYearsSinceEpoch();
+
+
+    h = "<tr><th>day 0</th><th>" + now + "</th></tr>";
+    l1 = "<tr><td>ms per day:</td><td>" + this.msPerDay + "</td></tr>";
+    l2 = "<tr><td>ms per week:</td><td>" +  this.msPerWeek + "</td></tr>";
+    l3 = "<tr><td>ms per leap year:</td><td>" +  this.msPerLeapYear + "</td></tr>";
+    l4 = "<tr><td>leap years:</td><td>" + this.totalLeptDays  + "</td></tr>";
+    l5 = "<tr><td>ms of days of year:</td><td>" + this.msOfYear  + "</td></tr>";
+    l6 = "<tr><td>ms today:</td><td>" + this.msToday  + "</td></tr>";
+    l7 = "<tr><td>day of year:</td><td>" + this.dayOfYear  + "</td></tr>";
+    l8 = "<tr><td>Year:</td><td>" + this.Year  + "</td></tr>";
+
+    log.debug( h + l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8);
+};
+tzDate.prototype.toString = function toString() {
+    // to do : implement this to include tz offset, dst and abbreviation.
+    return this.date.toString();
 }
+tzDate.prototype.leapYearsSinceEpoch = function leapYearsSinceEpoch() {
+    // Add 2 years to calculate 1st leap 1972
+    s = this.now.getTime();
+    this.totalLeptDays = Math.floor( (this.msPerYear * 2  + s) / this.msPerLeapYear);
+    return true;
+}
+tzDate.prototype.tz = function tz(tz) {
+    /* epoch timestamps are always in UTC.
+     * To calculate if an epoche is within
+     * a given time of the day,
+     * a simple calculation of
+     *      todayms = Date() modulo 24*60*60*1000
+     * must then have the tz offset and dst_offset
+     * values in seconds applied. e.g.
+     *      tz=1
+     *      dst=1
+     *      todayms += (tz+dst)*60*60*1000
+     * leap years and seconds need to be factored
+     * into the calculation also.
+     */
+
+    log.debug("dom = " + this.now.getDate() + "<BR>dow = " + this.now.getDay() );
+    this.days = Math.floor( this.now / msPerDay);
+    log.debug("Days since 1970 " + this.days + "<BR>tz" + this.now);
+    log.debug("ms today "+ (this.now - (this.days * this.msPerDay) ) );
+    log.debug("ms today "+ this.now.getHours() +":"+ this.now.getMinutes() );
+    log.debug("8h30m in ms = " + (20*60*60*1000 + 30*60*1000) );
+
+    log.debug('got tz:' + tz);
+    if (tz == undefined) {
+        tz = 1*60*60*1000; // force CET
+    }
+    this.tz = tz;
+    log.debug('set tz:' + tz);
+    return true;
+}
+tzDate.prototype.getTzTime = function getTzTime(){
+    return this.getTime() + this.tz;
+}
+
 /*
  * Commented out until it can be intergrated into the tzDate object.
     var output = "<b>" + tz.area + "/" + tz.location + "</b> : ";
@@ -443,6 +510,10 @@ function tzDate(date, tz, dst) {
 
 
 
+
+var period1 = new Period( );
+log.info( period1.toString() );
+
 /* An example of using splitfirst
 [area,loc] = splitfirst("America/Argentina/Buenos_Aires", "/");
 tzDate(zones[area][loc][0]);
@@ -455,271 +526,13 @@ for ( var area in zones ) {
 
 /* FROM PDA */
 
-function w(msg, tag)
-{
-tag = tag == undefined ? "p" : tag ;
-document.write( "<"+ tag +">"+ msg +"</"+ tag +">" );
-}
-
-w( "zeal", 'h1' );
-w("Test date="+new Date(1360341593000).toUTCString() );
-var TimeRange = function(){
-this.st =[0, 0, 0 ];
-this.et = [23,59,59];
-}
-
-TimeRange.prototype.insideRange = function() {
-
-l = ( this.st[0] * 3600 +
-this.st[1] * 60 +
-this.st[2] ) * 1000;
-
-u = ( this.et[0] * 3600 +
-this.et[1] * 60 +
-this.et[2] ) * 1000;
-
-return true;
-};
-
-
-TimeRange.prototype.toString = function() {
-return "" +  this.st[0] +
-":" +  this.st[1] +
-":" + this.st[2] +
-" - " +  this.et[0] +
-":" +  this.et[1] +
-":" +  this.et[2];
-};
-
-w('b4 time range');
-range = new TimeRange();
-
-TimeRange.prototype.tz = function(tz){
-    w('got tz:' + tz);
-    if (tz == undefined) {
-        tz = 1*60*60*1000; // force CET
-    }
-    this.tz = tz;
-    w('set tz:' + tz);
-    return true;
-};
-
-Date.prototype.getTzTime = function(){
-    return this.getTime() + this.tz;
-};
-
-n = new Date();
-w( n );
-w( "localtime: " + new Date( n.getTime() ) );
-
-w("LL "+ n.getTime() +'  ' + n.getTzTime() );
-
-w( "1h east gmt: " + new Date( n.getTzTime() ) );
-// i need to find out why this breaks.
-//n.tz();
-w( "4 1/2h east gmt: " + new Date( n.getTzTime() ) );
-
-w( range.toString() );
-
-
-// millisecond based calculations *********
-
-var msPerDay = 24*60*60*1000;
-var msPerWeek = 7 * msPerDay;
-var msPerYear = msPerDay * 365;
-var msPerLeapYear = msPerDay * ( 4 * 365 + 1);
-
-function LeapYearsSinceEpoch(s)
-{
-// Add 2 years to calculate 1st leap 172
-var l = (msPerYear * 2 ) + s
-return ( l - l % msPerLeapYear ) / msPerLeapYear;
-}
-
-// day based calculations *********
-
-var d0 = new Date(123456789000);
-
-var totalLeptDays = LeapYearsSinceEpoch( d0.getTime() ) ;
-
-
-msOfYear = d0.getTime() % msPerYear - ( totalLeptDays * msPerDay ) ;
-
-msToday = msOfYear % msPerDay;
-
-Year = ( msOfYear - msToday ) / msPerDay;
-
-dayOfYear = (msOfYear- msToday)  / msPerDay;
-
-h = "<tr><th>day 0</th><th>" + d0 + "</th></tr>";
-l1 = "<tr><td>ms per day:</td><td>" + msPerDay + "</td></tr>";
-l2 = "<tr><td>ms per week:</td><td>" +  msPerWeek + "</td></tr>";
-l3 = "<tr><td>ms per leap year:</td><td>" +  msPerLeapYear + "</td></tr>";
-l4 = "<tr><td>leap years:</td><td>" + totalLeptDays  + "</td></tr>";
-l5 = "<tr><td>ms of days of year:</td><td>" + msOfYear  + "</td></tr>";
-l6 = "<tr><td>ms today:</td><td>" + msToday  + "</td></tr>";
-l7 = "<tr><td>day of year:</td><td>" + dayOfYear  + "</td></tr>";
-l8 = "<tr><td>Year:</td><td>" + Year  + "</td></tr>";
-
-w( h + l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8, "table");
-
-
-function describeZoneRules(zr)
-{
-    for ( var x in zr )
-    {
-        w( x, "h3" );
-        var y_html="";
-        for ( var y in zr[x] )
-        {
-            y_html += "<LI>"+y+": "+ rules["US"] [x][y] + "</LI>\n";
+/*get utc time in ms add timezone offset get dateTime*/
+function dayToNumber(d) {
+    var days = [ "sun", "mon", "tue", "thu", "fri", "sat" ];
+    for ( var i = 0; i < days.length; i+=1) {
+        if (days[i] == d.toLowerCase() ) {
+            return i;
         }
-        w( y_html, 'ol' );
     }
+    return -1;
 }
-
-
-
-/* Test Date object with tz data values" */
-function tzDate(tz) {
-d = new Date();
-
-w(tz.area + "/" + tz.location , "h2");
-
-w("UTC Time : " + d.getTime() + " is " + d.toUTCString() );
-
-d.setTime( d.getTime() + (tz.gmt_off*1000) );
-w(d.getTime() + "  is " + d.toUTCString() + " with zone as " + tz.zone_format + " and refers to rule " + tz.rules );
-
-describeZoneRules(  rules[ tz.rules ] );
-
-}
-
-
-tzDate(zones["Africa"]["Algiers"][0]);
-tzDate(zones["Antarctica"]["Casey"][0]);
-tzDate(zones["Asia"]["Kabul"][0]);
-tzDate(zones["Australia"]["Darwin"][0]);
-tzDate(zones["Europe"]["London"][0]);
-/* 2 examples for America */
-tzDate(zones["EST"]["null"][0]);
-tzDate(zones["America"]["New_York"][0]);
-
-tzDate(zones["America"]["Argentina/Buenos_Aires"][0]);
-
-/*get utc time in ms
-add timezone offset
-get dateTime*/
-
-function dayToNumber(d){
- var days = [ "sun", "mon", "tue", "thu", "fri", "sat" ];
- for ( var i = 0; i < days.length; i+=1)
- {
-   if (days[i] == d.toLowerCase() )
-  {
-    return i;
-  }
-  return -1;
-  }
-}
-
-find_day = ["Sun",">=",7];
-
-w ( dayToNumber ( find_day[0]  ) );
-current_month = new Date(Date.UTC(2013,2,find_day[2]));
-w ( current_month.getDay() );
-
-/*
-function getFiles(filelist) {
-if filelist.class == String.class
-  if filelist == "all"
-   else
-    loadFile(filelist);
-if filelist.class == Array.class ....
-}
-*/
-
-w( find_day + " " + current_month );
-
-w( "end", 'h6' );
-
-
-/*
-epoch timestamps are always in UTC.  To calculate if an epoche is within a given time of the day, a simple calculation of
-todayms = Date() modulo 24*60*60*1000 must then have the tz offset and dst_offset values in seconds applied. e.g.
-tz=1
-dst=1
-todayms += (tz+dst)*60*60*1000
-leap years and seconds need to be factored into the calculation also.
-*/
-
-today = new Date();
-w("dom = " + today.getDate() +
-"<BR>dow = " + today.getDay()
-);
-
-
-days = Math.floor( today / msPerDay);
-
-w("Days since 1970 " + days + "<BR>tz"+today);
-w("ms today "+ (today - (days * msPerDay) ) );
-
-w("ms today "+ today.getHours() +":"+ today.getMinutes() );
-
-w("8h30m in ms = " + (18*60*60*1000 + 34*60*1000) );
-
-
-function makeRange( begin, end, duration, interval )
-{
-w( "BEGIN: "+ begin +
-"  END: " + end +
-"  DURATION: " + duration +
-"  INTERVAL: " + interval );
- return;
-}
-
-
-makeRange(0,23,1.5,2);
-// "00:00:00-23:59:59"
-
-range={
-t: ['00:00:00','23:59:59'],
-d:['mon',['saturday','sun']],
-w:[1,3,'4-8','1-40/2'],
-m:[1,3,'6-9','2-12/2'],
-y:[2013,'15-20','%2'],
-tz: "to do"
-};
-
-
-function CheckRange( arg )  {
-
-wd={ "mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":0 };
-
-w( "test range" );
-w( arg.keys );
-
-}
-
-
-// RUN Every Monday
-range({ run: true, d: "mon" });
-
-
-// DONT RUN between 1am-6am and 6pm-midnight
-range({run:false,t:"1:30-6,18-24", d:"sat-sun"});
-
-// RUN between 12p-1pm on 25/dec/2013
-range({t:"12-13", dm:"25", m:"dec", y:"2013"});
-
-// RUN first 2 weeks of each year
-range( {w:"1-2"} );
-
-
-// 1:30-7 fixed time range 1:30am - 7am
-// "0+1/3" fixed time duration with repetiton. Starting from midnight, apply a one hour period and repeat at 3h intervals.
-
-var s = ["7-9","0+1/3","0+1:30","5-7:00:01/2:30"];
-
-
-document.write('end');
